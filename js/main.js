bd91fb081c7ace12aac990c5d6f6489f83328967
ergashev-muhammad <ergashev.muhammad01@yandex.ru>
2022-02-23 22:31:33 +0500

    Responsive changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-const elFilmForm = findElement(".films__form");
-const elFilmsSearchInput = findElement('.films-search-input');
-const elFilmList = findElement(".films");
-const elFilmTemplate = findElement('#film-template').content;
+// Global variables
 
-const API_KEY = "845a7e9c"
+const elFilmForm = findElement(".films__form");
+const elFilmsSearchInput = findElement(".films-search-input");
+const elFilmList = findElement(".films");
+const elFilmTemplate = findElement("#film-template").content;
+const elPagination = findElement(".pagination")
+const elPrevBtn = findElement(".previous");
+const elNextBtn = findElement(".next");
+
+// API key
+
+const API_KEY = "845a7e9c";
+
+// Search query
+let searchQuery = "";
+
+// Page
+
+let page = 1;
+
+// Render films
 
 const renderFilms = (array, element) =>{
     
-    array.forEach((film)=>{
+    element.innerHTML = null;
+    
+    array.forEach((film) => {
         
         const filmTemplate = elFilmTemplate.cloneNode(true);
         
@@ -15,42 +33,82 @@ const renderFilms = (array, element) =>{
         filmTemplate.querySelector(".film__image").src = film.Poster;
         filmTemplate.querySelector(".film__year").textContent = film.Year;
         filmTemplate.querySelector(".film__type").textContent = film.Type;
-
+        
         element.appendChild(filmTemplate);
         
     });
     
 };
 
+// Async function
 
-async function getFilms(searchQuery){
-    const response = await fetch("http://www.omdbapi.com/?apikey=" + API_KEY + "&s=" + searchQuery);
+async function getFilms(){
     
-    const data = await response.json();
-    
-    if(data.Response === "True"){
-        renderFilms(data.Search, elFilmList)
-    } else {
+    try {
+        const response = await fetch("http://www.omdbapi.com/?apikey=" + API_KEY + "&s=" + searchQuery + "&page=" + page);
         
-        elFilmList.innerHTML = "The movie is not found"
-
+        const data = await response.json();
+        
+        const lastPage = Math.ceil(data.totalResults / 10);
+        
+        if(data?.Search){
+            elPagination.classList.add("pagination-open")
+        }
+        
+        if(data?.Search?.length > 0){
+            
+            renderFilms(data?.Search, elFilmList)   
+        }   
+        
+        if(page <= 1) {
+            elPrevBtn.disabled = true;
+        } else {
+            elPrevBtn.disabled = false;
+        }
+        
+        if(page === lastPage) {
+            elNextBtn.disabled = true;
+        } else {
+            elNextBtn.disabled = false;
+        }
+        
+    } catch (err) {
+        elFilmList.textContent = "The movie is not found"
+        
     }
 };
 
+// Film form
 
 elFilmForm.addEventListener('submit', (evt)=>{
     evt.preventDefault()
     
-    const searchQuery = elFilmsSearchInput.value.trim()
+    searchQuery = elFilmsSearchInput.value.trim()
     
     if(!searchQuery){
         return
     }
     
-    getFilms(searchQuery, elFilmTemplate);
+    getFilms();
     
     elFilmsSearchInput.value = null;
 });
+
+elPrevBtn.addEventListener("click", (evt) => {
+    
+    page--;
+    getFilms();
+})
+
+elNextBtn.addEventListener("click", (evt) => {
+    
+    page++;
+    getFilms();
+})
+
+getFilms();
+
+
 
 
 
